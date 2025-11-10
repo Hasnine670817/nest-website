22549d631eb8718d149a9f215d91871d0d73f932
Hasnine670817 <hasninetorun@gmail.com>
2025-11-10 23:18:19 +0600

    edited fruits.json

--- src/Sections/FeaturedCategories.jsx
+++ src/Sections/FeaturedCategories.jsx
@@ -1,14 +1,3 @@
-// import image1 from '../assets/images/Figure-7.png';
-// import image2 from '../assets/images/Figure-8.png';
-// import image3 from '../assets/images/Figure-6.png';
-// import image4 from '../assets/images/Figure-10.png';
-// import image5 from '../assets/images/Figure-1.png';
-// import image6 from '../assets/images/Figure-2.png';
-// import image7 from '../assets/images/Figure-3.png';
-// import image8 from '../assets/images/Figure-5.png';
-// import image9 from '../assets/images/Figure-9.png';
-// import image10 from '../assets/images/Figure-4.png';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -18,8 +7,6 @@ import { useEffect, useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
 
 const FeaturedCategories = () => {
-
-    // const slides = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10]
 
     const [fruits, setFruits] = useState([]);
 
